7841c630fe0d6dd34515f4c3d73f4e585e9868bd
On1zuma <wassi.mess@hotmail.com>
2022-12-09 14:34:50 +0100

    feat: url management

--- kernel/Controleur.php
+++ kernel/Controleur.php
@@ -2,32 +2,50 @@
 
 final class Controleur
 {
-    private $A_urlDecortique;
+    private $_A_urlDecortique;
 
-    public function __construct($S_controleur, $S_action)
+    public function __construct($S_url, $A_postParams)
     {
+        if ('/' == substr($S_url, -1, 1)) {
+            $S_url = substr($S_url, 0, strlen($S_url) - 1);
+        }
+        $A_urlDecortique = explode('/', $S_url);
+        //  Controleur / Action
+
+        if (!empty($A_urlDecortique[0])) {
+            $S_controleur = $A_urlDecortique[0];
+            if (!empty($A_urlDecortique[1])) {
+                $S_action = $A_urlDecortique[1];
+            } else {
+                $S_action = null;
+            }
+        } else {
+            $S_controleur = null;
+        }
+
+
         if (empty($S_controleur)) {
             // Nous avons pris le parti de préfixer tous les controleurs par "Controleur"
-            $this->A_urlDecortique['controleur'] = 'ControleurHelloworld';
+            $this->_A_urlDecortique['controleur'] = 'ControleurHelloworld';
         } else {
-            $this->A_urlDecortique['controleur'] = 'Controleur' . ucfirst($S_controleur);
+            $this->_A_urlDecortique['controleur'] = 'Controleur' . ucfirst($S_controleur);
         }
 
         if (empty($S_action)) {
             // L'action est vide ! On la valorise par défaut
-            $this->A_urlDecortique['action'] = 'defautAction';
+            $this->_A_urlDecortique['action'] = 'defautAction';
         } else {
             // On part du principe que toutes nos actions sont suffixées par 'Action'...à nous de le rajouter
-            $this->A_urlDecortique['action']  = $S_action . 'Action';
+            $this->_A_urlDecortique['action']  = $S_action . 'Action';
         }
-        var_dump($this->A_urlDecortique);
+        var_dump($A_urlDecortique);
     }
 
     // On exécute
     public function executer()
     {
         //fonction de rappel de notre controleur cible (ControleurHelloworld pour notre premier exemple)
-        call_user_func_array(array(new $this->A_urlDecortique['controleur'](),
-            $this->A_urlDecortique['action']), array());
+        call_user_func_array(array(new $this->_A_urlDecortique['controleur'](),
+            $this->_A_urlDecortique['action']), array());
     }
 }
