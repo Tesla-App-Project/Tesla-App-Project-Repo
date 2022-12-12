2d839564b12267a3acd947929dc8ae1756ad8504
Samaar Clément <clement.samaar@etu.univ-amu.fr>
2022-12-12 13:44:28 +0100

    fixing project

--- index.php
+++ index.php
@@ -1,37 +1,29 @@
 <?php
 
-// Ce fichier est le point d'entrée de votre application
-require_once 'model/ApiModel.php';
+// This file in the entry point of the application
+
 require 'kernel/AutoLoad.php';
 require_once __DIR__ . '/vendor/autoload.php';
 
 /*
- url pour notre premier test MVC Hello World,
- nous n'avons pas d'action précisée on visera celle par défaut
- index.php?ctrl=helloworld
- index.php?url=CTRL/ACTION
+to use our MVC, you have to enter inside your navbar
+    firstly : "index.php?url="
+    secondly : "index.php?url=ControllerName/ActionName"
 */
-
-$S_controleur = $_GET['ctrl'] ?? null;
-$S_action = $_GET['action'] ?? null;
-
-View::ouvrirTampon(); //  /kernel/View.php : on ouvre le tampon d'affichage, les contrôleurs qui appellent des vues les mettront dedans
-$O_controleur = new Controller($S_controleur, $S_action);
-
-// $S_controleur = $_GET['ctrl'] ?? null;
+// $S_controller = $_GET['ctrl'] ?? null;
 // $S_action = isset($_GET['action']) ? $_GET['action'] : null;
 
-$S_urlADecortiquer = isset($_GET['url']) ? $_GET['url'] : null;
-$A_postParams = isset($_POST) ? $_POST : null;
+$S_urlADecortiquer = $_GET['url'] ?? null;
+$A_postSettings = $_POST ?? null;
 
 // $S_url = isset($_GET['url']) ? $_GET['url'] : null;
 
-Vue::ouvrirTampon(); //  /kernel/Vue.php : on ouvre le tampon d'affichage, les contrôleurs qui appellent des vues les mettront dedans
-$O_controleur = new Controleur($S_urlADecortiquer, $A_postParams);
-$O_controleur->executer();
+View::ouvrirTampon(); //  /kernel/View.php : we open the display buffer, controllers called the views
+$O_controller = new Controller($S_urlADecortiquer, $A_postSettings);
+$O_controller->executer();
 
-// Les différentes sous-vues ont été "crachées" dans le tampon d'affichage, on les récupère
-$contenuPourAffichage = View::recupererContenuTampon();
+// The few sub views were put inside the display buffer, we get them
+$A_content = View::recupererContenuTampon();
 
-// On affiche le contenu dans la partie body du gabarit général
-View::montrer('gabarit', array('body' => $contenuPourAffichage));
+// We display the content in the template body
+View::montrer('template', array('body' => $A_content));
