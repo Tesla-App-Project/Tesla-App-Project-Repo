43915c0ae28ba15c8c4e8edd6a82aedad622c719
Manon CAYETANO <92310939+manoncayetano@users.noreply.github.com>
2022-12-12 14:22:35 +0100

    Update position.php

--- position.php
+++ position.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
         integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
         crossorigin="" />
-    <link rel="stylesheet" href="./assets/css/style.scss">
+    <link rel="stylesheet" href="../assets/css/style.scss">
     <style type="text/css">
         #map {
             /* la carte DOIT avoir une hauteur sinon elle n'apparaît pas */
