10d0e26e0687cb6c951443b56a36607d7ef00bee
Samaar Clément <clement.samaar@etu.univ-amu.fr>
2022-12-16 17:29:57 +0100

    feature : splitting view

--- controller/ControllerConnection.php
+++ controller/ControllerConnection.php
@@ -1,15 +1,13 @@
 <?php
 
-class ControllerHome
+class ControllerConnection
 {
     public function defautAction()
     {
         //http://localhost:8080/index.php?url=home
         $A_content =
             ['title' => 'Accueil',
-            'header' => 'HomeHeaderView',
-            'content' => 'HomeView',
-            'footer' => 'HomeFooterView'];
+            'content' => 'ConnectionView',];
 
         View::show('template', $A_content);
         //View::show('HomeView', array());
--- view/template.php
+++ view/template.php
@@ -1,3 +1,4 @@
+<?php if (isset($A_View)){ ?>
 <!doctype html>
 <html lang="fr">
 <head>
@@ -6,12 +7,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link href="./assets/css/style.scss" rel="stylesheet">
-    <script src="./assets/js/script.js"></script>
-    <title>Accueil</title>
+    <?php if (isset($A_View['style'])) echo '<link href="./assets/css/' . $A_View['style'] . '" rel="stylesheet">'; ?>
+    <title><?= $A_View['title'] ?></title>
 </head>
 <body>
 <?php
-if (isset($A_View)){
     if (isset($A_View['header'])) View::show('header/' . $A_View['header']);
     View::show($A_View['content']);
     if (isset($A_View['footer'])) View::show('footer/' . $A_View['footer']);
