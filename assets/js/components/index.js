7f53cb3a9ab71f019694f844fd99f8bfa55f4ed0
higginal <alec.m.higgins@gmail.com>
2023-01-24 14:29:07 +0100

    feat: new files for index

---
+++ assets/js/components/index.js
@@ -0,0 +1,44 @@
+import InputDetect from "../model_client/client_model";
+import ConnexionOnClick from "../assets/js/functions";
+import FanOnClick from "../assets/js/functions";
+
+let data = document.getElementById('charge_battery').dataset.action;
+var json = await InputDetect.GetDataFromServer(data);
+document.getElementById("charge_level").value = json.response.battery_level;
+document.getElementById('charge_battery').innerHTML = json.response.battery_level + "%";
+
+
+
+// Récupère tous les composants boutons et liens sous forme de tableau
+let boutons = document.querySelectorAll("button, a");
+
+// tout les composants boutons et liens
+boutons.forEach(bouton => {
+
+    let testdata = Object.keys(bouton.dataset).length==0;
+    // si data pas vide
+    if(!testdata){
+        bouton.addEventListener('click', async () => {
+
+            let data = bouton.dataset.action;
+            json = await InputDetect.GetDataFromServer(data);
+            
+            if (typeof(json)=="object"){
+                //document.getElementById('charge').innerHTML = "Niveau de charge : " + json.response.battery_level + "%";
+                if(data=="wake_up"){
+                    if(json[0].state="online"){
+                        ConnexionOnClick();
+                    }
+                }
+                else if(data=="auto_conditioning_start"){
+                    if(json.response.result == "true"){
+                        FanOnClick();
+                    }
+                }
+            }
+            else{
+                console.log("erreur !")
+            }
+        });
+    }
+});
--- assets/js/components/test.js
+++
@@ -1,4 +0,0 @@
-import CarInformation from "../model_client/car_information";
-let testcar = new CarInformation();
-let valeur = testcar.GetCarInformation();
-console.log("valeur =" +valeur);
