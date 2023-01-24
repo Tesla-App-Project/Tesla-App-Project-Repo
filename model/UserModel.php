181a35c54a96fcd7d5e42238977a461ef9d9b970
melisach <melisanoelchabour@gmail.com>
2023-01-24 15:43:36 +0100

    features MAJ model et controller 2

--- model/UserModel.php
+++ model/UserModel.php
@@ -1,28 +1,28 @@
 <?php
 
 $filepath = realpath(dirname(__FILE__));
-require_once ($filepath."/../kernel/Database.php");
+require_once ($filepath."/../kernel/DatabaseUser.php");
 
 final class UserModel
 {
     public function updateUser($user_mail,$user_first_name,$user_username,$lastname)
     {
-        $db = new Database();
+        $db = new DatabaseUser();
         //UPDATE user
 
-         return $db->queryUpdateUserAction(['email' => $user_mail, 'firstname' => $user_first_name,'username'=>$user_username,'lastname' => $lastname]);
+        $db->queryUpdateUserAction(['email' => $user_mail, 'firstname' => $user_first_name,'username'=>$user_username,'lastname' => $lastname]);
     }
 
     public function getUser($user_mail,$user_password)
     {
-        $db = new Database();
+        $db = new DatabaseUser();
         //login user
         $users = $db->queryGetUserAction(['email' => $user_mail, 'password' => $user_password]);
     }
 
     public function newUser($user_first_name,$user_last_name,$user_username,$user_password,$user_mail,$user_token)
     {
-        $db = new Database();
+        $db = new DatabaseUser();
         //CREATE user
         $users = $db->queryCreateUserAction(
             [
